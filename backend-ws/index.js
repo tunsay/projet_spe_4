07521fcb096f31ead5fb0e54b01e0551078cb8ef
Valcasara Bryan <bryan.valcasara@livecampus.tech>
2025-10-28 17:03:28 +0100

    initiate ws server

---
+++ backend-ws/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "http";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  
+  socket.on("message", (msg) => {
+    console.log("message received: " + msg);
+    socket.send("Echo: " + msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+httpServer.listen(process.env.PORT || 3005, () => {
+  console.log("process.env.PORT", process.env.PORT);
+});
+
+
+export default app;
