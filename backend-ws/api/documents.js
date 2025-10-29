ddbe3a524926922a15e5e2271e0610c3e3d219fb
Valcasara Bryan <bryan.valcasara@livecampus.tech>
2025-10-29 16:53:01 +0100

    fix docker

--- backend-ws/api/documents.js
+++ backend-ws/api/documents.js
@@ -1,7 +1,7 @@
 
 export default {
     getById: async (user, documentId) => {
-        const response = await fetch(process.env.API_URL + `documents/` + documentId, {
+        const response = await fetch(process.env.API_INTERNAL_URL + `/api/documents/` + documentId, {
             credentials: 'include',
             headers: {
                 "Cookie": `token=${user.token}`
@@ -13,7 +13,7 @@ export default {
         return response.json();
     },
     getByIdPermission: async (user, documentId) => {
-        const response = await fetch(process.env.API_URL + `documents/${documentId}/permissionByUser` + `?userId=${user.id}`, {
+        const response = await fetch(process.env.API_INTERNAL_URL + `/api/documents/${documentId}/permissionByUser` + `?userId=${user.id}`, {
             credentials: 'include',
             headers: {
                 "Cookie": `token=${user.token}`
